365608e19a0e1e03c0c4379c4da13111042faae5
Lachie <lachie.duncan12@gmail.com>
2024-05-06 11:22:46 +1000

    added suspense boundary around searchparams hook

--- app/(auth)/delete-account/page.tsx
+++ app/(auth)/delete-account/page.tsx
@@ -29,7 +29,7 @@ export default function DeleteAccountPage() {
 
             setUser(data.user);
         }
-    }, []);
+    }, [router, user, supabase.auth]);
 
     const onDelete = async () => {
         if (!user) return;
