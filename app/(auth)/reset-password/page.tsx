251e43f1686d2986f425912ff8189804029f5653
Lachie <lachie.duncan12@gmail.com>
2024-05-23 17:02:01 +1000

    added suspense boundary for useSearchParams

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,52 +1,21 @@
-"use client";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
 
-import { useSearchParams } from "next/navigation";
-
-import { createClient } from "@/utils/supabase/client";
-
+import { Input } from "@/components/ui/input";
 import Logo from "@/logo";
 
-import ResetPasswordForm from "./components/reset-password-form";
-import SendPasswordResetEmail from "./components/send-reset-password-email";
+import ResetPasswordPage from "./components/reset-password-page";
 
-export default function PasswordReset() {
-    const [supabaseClient, setSupabaseClient] = useState<ReturnType<typeof createClient>>(createClient());
-    const [hasSession, setHasSession] = useState<boolean>(false);
-    const [error, setError] = useState<boolean>(false);
-    const searchParams = useSearchParams();
-    
-    useEffect(() => {
-        getSessionFromSearchParams();
+const FormPlaceholder = () => {
+    return (
+        <div className='sm:w-[360px] w-[240px] flex flex-col items-stretch gap-6'>
+            <div>Email Address</div>
+            <Input />
+            <div>We will send you an email to change your password.</div>
+        </div>
+    )
+}
 
-        async function getSessionFromSearchParams() {
-            if (searchParams.get('access_token') && searchParams.get('refresh_token')) {
-                const { data, error } = await supabaseClient.auth.setSession({
-                    access_token: searchParams.get('access_token')!,
-                    refresh_token: searchParams.get('refresh_token')!
-                });
-
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-
-                setHasSession(true);
-            }
-
-            if (searchParams.get('code')) {
-                // supabase client retrieves session automatically
-                // see https://supabase.com/docs/guides/auth/passwords?queryGroups=flow&flow=pkce&queryGroups=language&language=js
-                setHasSession(true);
-            }
-
-            if (searchParams.get('error')) {
-                // TO DO: improve error feedback
-                setError(true);
-            }
-        }
-    }, [searchParams, supabaseClient, setHasSession, setError]);
-
+export default function Page() {
     return (
         <main className='h-[100vh] flex items-center justify-center bg-yellow-100 p-12'>
             <div className='h-full flex flex-col items-center gap-6'>
@@ -54,18 +23,9 @@ export default function PasswordReset() {
 
                 <h3 className='text-center text-xl font-semibold'>Reset My Password</h3>
                 
-                {error && (
-                    <div className='text-center text-red-400'>
-                        <p>Sorry, that link has expired or something went wrong.</p>
-                        <p>Please try again.</p>
-                    </div>
-                )}
-
-                {hasSession? (
-                <ResetPasswordForm supabase={supabaseClient} />
-                ) : (
-                <SendPasswordResetEmail supabase={supabaseClient} />
-                )}
+                <Suspense fallback={<FormPlaceholder />}>
+                    <ResetPasswordPage />
+                </Suspense>
             </div>
         </main>
     )
